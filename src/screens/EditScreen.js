cc6f3c40b134f60dad7b2ec279703936c7ef7231
Ciri Ryan <cirijaye@yahoo.com>
2021-10-13 14:15:33 -0700

    display photo on EditScreen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { PESDK, PhotoEditorModal, Configuration } from 'react-native-photoeditorsdk';
+import * as SecureStore from 'expo-secure-store';
 
 const EditScreen = ({ navigation }) => {
+	const [ lastImage, setLastImage ] = useState(null);
+
+	useEffect(() => {
+		SecureStore.getItemAsync('lastImage').then((image) => {
+			if (image) {
+				setLastImage(JSON.parse(image));
+			}
+		});
+	});
+
 	return (
 		<SafeAreaView style={styles.safeArea}>
 			<View style={styles.container}>
 				<TouchableOpacity style={styles.navigationStyle} onPress={() => navigation.navigate('Home')}>
 					<Feather name="x" size={32} color="white" />
 				</TouchableOpacity>
+				<Text>{JSON.stringify(lastImage)}</Text>
+				{lastImage && (
+					<PhotoEditorModal
+						onExport={(e) => {
+							SecureStore.setItemAsync('lastImage', JSON.stringify({ ...lastImage, uri: e.image }));
+						}}
+						visible={true}
+						image={lastImage.uri}
+					/>
+				)}
 			</View>
 		</SafeAreaView>
 	);
@@ -23,7 +45,7 @@ const styles = StyleSheet.create({
 	container: {
 		flexDirection: 'column',
 		flex: 1,
-        backgroundColor: '#212121'
+		backgroundColor: '#212121'
 	},
 	navigationStyle: {
 		flexDirection: 'row',
@@ -31,7 +53,7 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		height: 80,
 		backgroundColor: '#212121',
-        alignSelf: 'flex-end'
+		alignSelf: 'flex-end'
 	}
 });
 

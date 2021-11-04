2fe995f187dc103e272f28429038837f3751d2d1
Ciri Ryan <cirijaye@yahoo.com>
2021-11-04 08:26:03 -0700

    make responsive new style home page and logo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,34 +1,41 @@
 import React from "react";
 import { Image, StyleSheet, View, TouchableOpacity, Text, SafeAreaView, StatusBar } from "react-native";
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo7.png';
 
 
 const HomeScreen = ({ navigation }) => {
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 	  width: Dimensions.get('window').width
+	// 	};
   return (
 	// <SafeAreaView style={styles.safeArea}>
 	<>
 	<SafeAreaView style={styles.safeAreaTop} />
 		<SafeAreaView style={styles.safeAreaBottom}>
             <StatusBar barStyle="light-content" backgroundColor="#189BF3" />
+	{/* navigation to camera screen from logo */}
 			<TouchableOpacity style={styles.touchableStyle} onPress={() => navigation.navigate('Camera')}>
 			<View style={styles.ViewStyle}>
 				<Image source={logo} style={styles.logo} />
-				<Text style={styles.appName}>Presize Camera</Text>
 			</View>	
 			</TouchableOpacity>
-
+	{/* account buttons */}
 			<View style={styles.accountStyle}>
 				<TouchableOpacity 
-					style={styles.loginStyle}
+					style={styles.buttonStyle}
 					onPress={() => navigation.navigate("Login")}
 				>
-					<Text style={styles.loginTextStyle}>Sign In</Text>
+					<Text style={styles.TextStyle}>Sign In</Text>
 				</TouchableOpacity>
+
 				<TouchableOpacity 
-					style={styles.createStyle}
+					style={styles.buttonStyle}
 					onPress={() => navigation.navigate("Create")}
+					// {this.state.width < 320 ? <Text>width of the past</Text> : <Text>how big is big enough?</Text>}
 				>
-					<Text style={styles.createTextStyle}>Sign Up</Text>
+					<Text style={styles.TextStyle}>Sign Up</Text>
 				</TouchableOpacity>
 			</View>
 		
@@ -40,19 +47,15 @@ const HomeScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   	logo: {
-		// color: '#fff',
+		flex: 0,
 		backgroundColor: '#189BF3',
-		width: '30%',
-		height: '20%',
+		width: '64%',
+		height: '31%',
 		marginBottom: 25,
 		marginTop: 55
 	},
-	appName: {
-		color: 'white',
-		fontSize: 35,
-		fontWeight: '700'
-	},
 	ViewStyle: {
+		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
 		backgroundColor: '#189BF3',
@@ -69,60 +72,36 @@ const styles = StyleSheet.create({
 	
 	safeAreaBottom: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#189BF3',
 		alignItems: 'center',
 	  },
 	safeAreaTop: {
 		flex: 0,
 		backgroundColor: '#189BF3'
 	  },
-	buttonsStyle: {
-		flex: 1,
-		justifyContent: 'center'	
+	buttonStyle: {
+		flex: 0,
+		borderColor: 'white',
+		backgroundColor: '#189BF3',
+		borderWidth: 1,
+		aspectRatio: 22/3,
+		height: 50,
+		borderRadius: 2,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginBottom: 30,
 	},
 
 	accountStyle: {
-	
+		flex: 1,
 		alignContent: 'center',
-		justifyContent: 'center'
-		
+		justifyContent: 'center'	
 	},
 	
-	loginStyle: {
-		borderColor: 'white',
-		backgroundColor: '#189BF3',
-		borderWidth: 1,
-		height: 45,
-		width: 300,
-		borderRadius: 2,
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginBottom: 30,
-		marginRight: 15
-	
-	},
-	loginTextStyle: {
+	TextStyle: {
 		color:	'#fff',
-		fontSize: 30
-	},
-	createStyle: {
-		borderColor: 'white',
-		backgroundColor: '#189BF3',
-		borderWidth: 1,
-		height: 45,
-		width: 300,
-		borderRadius: 2,
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginBottom: 30,
-		marginRight: 15
-	
-	},
-	createTextStyle: {
-		color:	'#fff',
-		fontSize: 30
+		fontSize: 25
 	}
-
 
 });
 

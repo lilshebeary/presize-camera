775631cd084cb7e070459858a113110a09af2a5a
Ciri Ryan <cirijaye@yahoo.com>
2022-05-18 13:52:51 -0700

    add create folder screen and add button

---
+++ src/screens/CreateFolderScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const CreateFolderScreen = () => {
+  return (
+    <View>
+      <Text>CreateFolderScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+
+    
+})
+
+
+export default CreateFolderScreen;
